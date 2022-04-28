ea7cddd3ca5da82276c1157591f7ce46acca8880
Stéphane <stephane1920@gmail.com>
2022-04-28 10:46:55 +0200

    prepare environment for join all project API + Front

--- api/server/database/connection.js
+++ api/server/database/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
+const dotEnv = require('dotenv')
+dotEnv.config()
+
 const databaseUrl =
   process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    await mongoose.connect(databaseUrl, {useNewUrlParser: true, useUnifiedTopology: true})
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
