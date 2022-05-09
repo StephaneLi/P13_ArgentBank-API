37b0ce36e92100e8afa014ee8be5afb8ca5cd9d7
Stéphane <stephane1920@gmail.com>
2022-05-09 17:37:55 +0200

    add default value for API_HOST_VARIABLE

--- react_app/src/config/config.ts
+++ react_app/src/config/config.ts
@@ -1,7 +1,7 @@
 import { IConfig } from "../interfaces/Config.intf"
 
 const Config: IConfig = {
-  API_HOST_PATH: process.env.REACT_APP_API_HOST,
+  API_HOST_PATH: process.env.REACT_APP_API_HOST || 'http://localhost:3000/',
   API_MOCK: process.env.REACT_APP_DEPLOY_MOCK === 'true'
 }
 
